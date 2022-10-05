8da417ec74ef7e7172b14c7bde461d7a3838d4fd
BeautyVika <vikachk54@gmail.com>
2022-10-05 09:54:41 +0300

    added error message

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -19,11 +19,13 @@ type TaskType ={
 const TodoList =(props: PropsType)=>{
 
     let [title, setTitle] = useState('')
+    let [error, setError] = useState<null | string>(null)
 
     const onChangeHandler =(event: ChangeEvent<HTMLInputElement>)=>{
         setTitle(event.currentTarget.value)
     }
     const onKeyPressHandler =(event: KeyboardEvent<HTMLInputElement>) =>{
+        setError(null)
         if(event.key === 'Enter'){
             addTaskHandler()
         }
@@ -32,6 +34,8 @@ const TodoList =(props: PropsType)=>{
         if (title.trim() !== ''){
             props.addTask(title.trim())
             setTitle('')
+        }else {
+            setError('Title is required')
         }
     }
     const onChangeFilter = (filter: FilterType) => {
@@ -60,8 +64,11 @@ const TodoList =(props: PropsType)=>{
             <div>
                 <input value={title}
                        onChange={onChangeHandler}
-                       onKeyDown={onKeyPressHandler}/>
+                       onKeyDown={onKeyPressHandler}
+                       className={error ? 'error-message' : ''}/>
+
                 <button onClick={addTaskHandler}>+</button>
+                {error && <div className='error-message'>{error}</div>}
             </div>
             <ul>
                 {mapTasks}
